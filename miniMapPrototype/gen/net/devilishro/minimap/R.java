31b0ec7b25ec4f13570cd9e4f43f3feba036a4df
treeRain48 <david16@mail.usf.edu>
2014-02-27 19:42:09 -0500

    Tempy

--- miniMapPrototype/gen/net/devilishro/minimap/R.java
+++ miniMapPrototype/gen/net/devilishro/minimap/R.java
@@ -75,7 +75,8 @@ public final class R {
         public static final int activity_notification=0x7f030005;
         public static final int activity_player_state=0x7f030006;
         public static final int eventadd=0x7f030007;
-        public static final int view_event=0x7f030008;
+        public static final int state=0x7f030008;
+        public static final int view_event=0x7f030009;
     }
     public static final class menu {
         public static final int event_join=0x7f080000;
--- miniMapPrototype/src/net/devilishro/minimap/Player_state.java
+++ miniMapPrototype/src/net/devilishro/minimap/Player_state.java
@@ -1,17 +1,68 @@
 package net.devilishro.minimap;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Player_state extends Activity {
 
+	ArrayList<String> listy;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_player_state);
+		listy = new ArrayList<String>();
+		setList();
+		ListView one = (ListView) this.findViewById(R.id.listView1);
+		one.setAdapter(new CoolAdapter(this, listy));
 	}
 
+	private void setList() {
+		listy.add("Match 1");
+		listy.add("Match 2");
+		listy.add("Match 2");
+		listy.add("Match 4");
+		listy.add("Match 5");
+	}
+	
+	private static class CoolAdapter extends ArrayAdapter<String> {
+		Context context;
+		ArrayList<String> b;
+
+		public CoolAdapter(Player_state player_state, ArrayList liste) {
+			super(player_state, R.id.listView1, liste);
+			b = liste;
+			context = player_state;
+		}
+
+		@Override
+		public View getView(int pos, View convert, ViewGroup parent) {
+			String temp;
+			if (convert == null)
+				convert = View.inflate(context, R.layout.state,
+						null);
+			try {
+				temp = b.get(pos);
+			} catch (Exception e) {
+				temp = null;
+			}
+
+			if (temp != null)
+				((TextView) convert.findViewById(R.id.textView1)).setText(temp);
+			return convert;
+		}
+
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
