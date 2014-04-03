2a11dc153b8267423b838d29813ae536f92249ab
xadneil <dflaws1@mail.usf.edu>
2014-04-03 16:41:53 -0400

    removed files

--- miniMap/src/edu/usf/cse/minimap/Opcode.java
+++
@@ -1,5 +0,0 @@
-package edu.usf.cse.minimap;
-
-public interface Opcode {
-    public byte getValue();
-}
--- miniMap/src/edu/usf/cse/minimap/PacketHandler.java
+++
@@ -1,8 +0,0 @@
-package edu.usf.cse.minimap;
-
-import java.net.Socket;
-
-public abstract class PacketHandler {
-
-    public abstract void handlePacket(Packet packet, Socket s);
-}
